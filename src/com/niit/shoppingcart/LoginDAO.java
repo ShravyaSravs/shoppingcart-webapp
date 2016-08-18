33fb76b733d50e377ffb1d8102ffdc831dab600c
shravya <shravya@LAPTOP-RB2O83EE>
2016-08-18 14:48:05 +0530

    shoppingcart web app

---
+++ src/com/niit/shoppingcart/LoginDAO.java
@@ -0,0 +1,17 @@
+package com.niit.shoppingcart;
+
+public class LoginDAO {
+	public Boolean isValidUser(String userid,String pwd){
+		if(userid.equals("niit")&&pwd.equals("niit"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+
+
+}
